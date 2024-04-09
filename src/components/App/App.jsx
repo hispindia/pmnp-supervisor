b096c0c2f7ddab0ca0049b884b0ff9803f46a8ad
Hoang Bui <jasonbui692@gmail.com>
2024-04-10 00:22:16 +0700

    Remove unused code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import ControlBar from '../ControlBar/ControlBar';
-import RegisteredTeiListContainer from '../../containers/RegisteredTeiList';
+import { lazy, Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+import RegisteredTeiListContainer from "../../containers/RegisteredTeiList";
+import ControlBar from "../ControlBar/ControlBar";
 
-import MainForm from '../../containers/MainForm';
-import Report from '../../modules/Report/containers/Layout';
-import withOrgUnitRequired from '../../hocs/withOrgUnitRequired';
-import OrgUnitRequired from '../../skeletons/OrgUnitRequired';
+import MainForm from "../../containers/MainForm";
+import withOrgUnitRequired from "../../hocs/withOrgUnitRequired";
+import OrgUnitRequired from "../../skeletons/OrgUnitRequired";
 
-// import RegisteredTeiFormContainer from "../../containers/RegisteredTeiForm";
-
-// const { appContentContainer } = styles;
+const Report = lazy(() => import("../../modules/Report/containers/Layout"));
 
 const App = () => {
-    return (
-        <>
-            <Route path="/" render={() => <ControlBar />} />
-            <Switch>
-                <Route path="/list" component={RegisteredTeiListContainer} />
-                <Route path="/form" component={MainForm} />
-                {/* <Route path="/form1" component={FamilyMemberFormContainer} /> */}
-                {/* <Route path="/form1" component={FMLayout} /> */}
+  return (
+    <>
+      <Route path="/" render={() => <ControlBar />} />
+      <Switch>
+        <Route path="/list" component={RegisteredTeiListContainer} />
+        <Route path="/form" component={MainForm} />
+        {/* <Route path="/form1" component={FamilyMemberFormContainer} /> */}
+        {/* <Route path="/form1" component={FMLayout} /> */}
 
-                {/*<Route*/}
-                {/*  path="/form"*/}
-                {/*  render={() => <FormContainer programMetadata={programMetadata} />}*/}
-                {/*/>*/}
-                <Route
-                    path="/report"
-                    component={withOrgUnitRequired(OrgUnitRequired)(Report)}
-                />
-            </Switch>
-        </>
-    );
+        {/*<Route*/}
+        {/*  path="/form"*/}
+        {/*  render={() => <FormContainer programMetadata={programMetadata} />}*/}
+        {/*/>*/}
+        <Route
+          path="/report"
+          component={
+            <Suspense fallback={<div>Loading...</div>}>
+              {withOrgUnitRequired(OrgUnitRequired)(<Report />)}
+            </Suspense>
+          }
+        />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
