515196515a9d78a2b24f80b38f8c061f2d2e3a67
anhhuy294 <anhhuy2099@gmail.com>
2024-06-05 14:58:55 +0700

    fix save pdf for report

--- src/modules/Report/services/hightchart.js
+++ src/modules/Report/services/hightchart.js
@@ -1,16 +1,16 @@
 const EXPORT_HIGHCHART_ENDPOINT = "https://export.highcharts.com";
 
-const exportHighChart = async options => {
+const exportHighChart = async (options) => {
   return fetch(EXPORT_HIGHCHART_ENDPOINT, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(options)
+    body: JSON.stringify(options),
   });
 };
 
-const toImageData = async blob =>
+const toImageData = async (blob) =>
   new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.onloadend = () => resolve(reader.result);
@@ -18,4 +18,33 @@ const toImageData = async blob =>
     reader.readAsDataURL(blob);
   });
 
-export { toImageData, exportHighChart };
+const convertSVGToBase64 = (svgString) => {
+  return new Promise(async (resolve, reject) => {
+    const parser = new DOMParser();
+    const svgElem = parser.parseFromString(
+      svgString,
+      "image/svg+xml"
+    ).documentElement;
+
+    // Serialize the SVG element to string
+    const svgXml = new XMLSerializer().serializeToString(svgElem);
+    const blob = new Blob([svgXml], { type: "image/svg+xml" });
+    const svgBase64 = await toImageData(blob);
+
+    const img = new Image();
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+
+      const base64Image = canvas.toDataURL("image/png"); // 'image/jpeg' nếu bạn muốn định dạng khác
+      resolve(base64Image);
+    };
+    img.onerror = (error) => reject(error);
+    img.src = svgBase64;
+  });
+};
+
+export { toImageData, exportHighChart, convertSVGToBase64 };
