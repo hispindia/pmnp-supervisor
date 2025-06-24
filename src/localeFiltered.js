35fbc04f42f2e92d88e80a05edef24d77ece09be
huynguyen294 <anhhuy2099@gmail.com>
2025-06-24 11:53:33 +0700

    fix(locale): remove unuse locale

---
+++ src/localeFiltered.js
@@ -0,0 +1,161 @@
+const locale = {
+  select: {
+    en: "Select",
+    lo: "ເລືອກ",
+  },
+  addNewFamily: {
+    en: "Add New Household",
+    lo: "ເພີ່ມຄອບຄົວໃໝ່",
+  },
+  barangay: {
+    en: "Barangay",
+    lo: "ລົງທະບຽນບ້ານ",
+  },
+  familyRegistration: {
+    en: "Household Registration",
+    lo: "ລົງທະບຽນຄອບຄົວ",
+  },
+  householdMembers: {
+    en: "Household Members",
+    lo: "ສະມາຊິກຄອບຄົວ (ບ 1 ກ)",
+  },
+  interviewDetails: {
+    en: "Scorecard Interview",
+    lo: "Scorecard Interview",
+  },
+  interviewResult: {
+    en: "Interview Result",
+    lo: "Interview Result",
+  },
+  "Household Survey": {
+    en: "Household Survey",
+    lo: "ການຜົນການຄອບຄົວ",
+  },
+  close: {
+    en: "Close",
+    lo: "ປິດ",
+  },
+  exit: {
+    en: "Exit",
+    lo: "ອອກ",
+  },
+  formNo: {
+    en: "Form No.",
+    lo: "ຟອມເລກທີ",
+  },
+  some: { en: "Some", lo: "ມີຄວາມຍຸ້ງຍາກໜ້ອຍໜື່ງ" },
+  alot: { en: "A lot", lo: "ມີຄວາມຍຸ້ງຍາກຫຼາຍ" },
+  lastUpdated: { en: "Last Updated", lo: "ປັບຄັ້ງລ່າສຸດ" },
+  rowsPerPage: { en: "Rows Per Page", lo: "ຈຳນວນແຖວຕໍ່ໜ້າ" },
+  next: { en: "NEXT", lo: "ຕໍ່ໄປ" },
+  yes: { en: "Yes", lo: "ມີ" },
+  no: { en: "No", lo: "ບໍ່ມີ" },
+  select: { en: "Select", lo: "ເລືອກ" },
+  thisFieldIsRequired: {
+    en: "This field is required",
+    lo: "ທ່ານຕ້ອງປ້ອນຂໍ້ມູນເຂົ້າຈຸດນີ້",
+  },
+  addNewMember: { en: "Add New Member", lo: "ເພີ່ມສະມາຊິກໃໝ່" },
+  addNewInterview: { en: "Add New Interview", lo: "Add New Interview" },
+  save: { en: "Save", lo: "ບັນທຶກ" },
+  submit: { en: "Submit" },
+  action: { en: "Action", lo: "Action" }, //THIS IS IN FAMILY LIST (action column lebel )
+  familyMemberDetails: {
+    en: "Family Member Details",
+    lo: "ລາຍລະອຽດສະມາຊິກຄອບຄົວ",
+  }, //jthomas: repeat from line 2
+  deleteDialogTitle: {
+    en: "Are you sure you want to delete this Family?",
+    lo: "ທ່ານໝັ້ນໃຈແລ້ວບໍ່ວ່າທ່ານຕ້ອງການລົບຄອບຄົວນີ້?",
+  },
+  edit: { en: "Edit", lo: "ແກ້ໄຂ" },
+  deleteDialogContent: {
+    en: "This process cannot be undone!",
+    lo: "ຂະບວນການນິ້ແມ່ນບໍ່ສາມາດກູ້ຂໍ້ມູນກັບຄືນມາໄດ້ອີກ!",
+  },
+  cancel: { en: "Cancel", lo: "ຍົກເລີກ" },
+  delete: { en: "Delete", lo: "ລົບ" },
+  lastUpdated: { en: "Last Updated", lo: "ປັບຄັ້ງລ່າສຸດ" },
+  for: {
+    en: "For",
+    lo: "ສຳລັບ",
+  },
+  year: {
+    en: "Year",
+    lo: "ປີ",
+  },
+  month: {
+    en: "Month",
+    lo: "ເດືອນ",
+  },
+  syncChangedData: {
+    en: "Sync changed data",
+    lo: "ສົ່ງຂໍ້ມູນ",
+  },
+  pleasePushChangedData: {
+    en: "Please sync changed data.",
+    lo: "ແຈ້ງເຕືອນ.",
+  },
+  somethingWentWrong: {
+    en: "Something went wrong",
+    lo: "ມີບາງຢ່າງຜິດພາດ",
+  },
+  importSuccess: {
+    en: "Import success",
+    lo: "ນຳເຂົ້າສຳເລັດ",
+  },
+  import: {
+    en: "Import",
+    lo: "ນຳເຂົ້າ",
+  },
+  noImportFileSelected: {
+    en: "No import file selected",
+    lo: "ບໍ່ມີໄຟຟ້ານຳເຂົ້າ",
+  },
+  importExcel: {
+    en: "Import Excel",
+    lo: "ນຳເຂົ້າ Excel",
+  },
+  pleaseInstallApp: {
+    en: "Please install app to use this function.",
+    lo: "Please install app to use this function.",
+  },
+  preparingForOfflineMode: {
+    en: "Preparing for offline mode",
+    lo: "ກຳລັງກຽມຄວາມພ້ອມເພື່ອໃຊ້ແບບບໍ່ຕ້ອງການເນັດ",
+  },
+  warning: {
+    en: "Warning",
+    lo: "ແຈ້ງເຕືອນ",
+  },
+  sync: {
+    en: "Sync",
+    lo: "ລົງທະບຽນ",
+  },
+  exportExcel: {
+    en: "Export Excel",
+    lo: "ເອັກສະປອດເປັນເອັກເຊ້ວ",
+  },
+  online: {
+    en: "Online",
+    lo: "ອອນໄລນ໌",
+  },
+  offline: {
+    en: "Offline",
+    lo: "ອອບໄລນ໌",
+  },
+  download: {
+    en: "Download",
+    lo: "ດາວໂຫຼດ",
+  },
+  list: {
+    en: "List",
+    lo: "ໂຫຼດລາຍຊື່ຄືນໃໝ່",
+  },
+  downloadOfflineHelper: {
+    en: "Please select barangay to use offline then click download.",
+    lo: "ກະລຸນາເລືອກໂຄງຮ່າງການຈັດຕັ້ງເພື່ອນຳໃຊ້ໃນໂຫມດອອບລາຍ, ຫລັງຈາກນັ້ນຄິກໃສ່ປຸ່ມດາວໂຫຼດ.",
+  },
+};
+
+export default locale;
