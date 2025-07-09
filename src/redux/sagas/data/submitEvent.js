2a41c4353c6eaab3ef4458305453d8ae006dfe09
huynguyen294 <anhhuy2099@gmail.com>
2025-07-09 17:11:48 +0700

    add error message when push failed

--- src/redux/sagas/data/submitEvent.js
+++ src/redux/sagas/data/submitEvent.js
@@ -1,3 +1,4 @@
+import i18n from "i18next";
 import { call, put, select, takeLatest } from "redux-saga/effects";
 import { dataApi } from "../../../api";
 import { SUBMIT_EVENT, SUBMIT_EVENTS } from "../../types/data/tei";
@@ -5,6 +6,7 @@ import { SUBMIT_EVENT, SUBMIT_EVENTS } from "../../types/data/tei";
 import { getTei, getTeiError, loadTei } from "../../actions/data/tei";
 
 import * as eventManager from "@/indexDB/EventManager/EventManager";
+import { notification } from "antd";
 
 function* handleSubmitEvent({ event, refreshTei = true }) {
   const { offlineStatus } = yield select((state) => state.common);
@@ -20,18 +22,27 @@ function* handleSubmitEvent({ event, refreshTei = true }) {
     } else {
       yield call(dataApi.pushEvents, { events: [event] });
     }
+
+    if (refreshTei) yield put(getTei(currentTei.trackedEntity));
   } catch (e) {
     console.error("handleSubmitEvent", e);
+    const result = yield e.json();
+    const message = result.validationReport.errorReports.map((errorReport) => errorReport.message).join("\n");
+    notification.error({
+      message: i18n.t("Error"),
+      description: message || "Save event failed!",
+      placement: "bottomRight",
+      duration: 0,
+    });
+
     // comment because cannot refresh tei
     // yield put(getTeiError("save event failed!"));
   } finally {
     // refresh TEI
     yield put(loadTei(false));
-
     /**
      * this line should be placed after the loadTei(false) to keep the loading state
      *  */
-    if (refreshTei) yield put(getTei(currentTei.trackedEntity));
   }
 }
 
